dbdc687d23ba77fb12a003280d94eb05aab9bf35
Z - work <zeeshans@dataom.co.nz>
2018-06-15 08:03:58 +1200

    Jar to war output conversion

--- src/main/java/com/example/test/demo/DemoApplication.java
+++ src/main/java/com/example/test/demo/DemoApplication.java
@@ -2,11 +2,19 @@ package com.example.test.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	// For building WAR instead of JAR
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
 	}
 }
